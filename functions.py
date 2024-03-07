d232d0d5ad2ae0548fd0db30a66dbfe2187bdfde
Rodrigo Reis <suporteproc@alema.local>
2024-03-07 11:56:36 -0500

    ajustando a criacao da pasta

--- functions.py
+++ functions.py
@@ -1,4 +1,3 @@
-# import pandas as pd
 from pathlib import Path
 
 def mesclar_panilhas(vendas, lojas):
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from functions import *
-
+from pathlib import Path
 
 
 
@@ -9,6 +9,8 @@ def main():
     emails_df = pd.read_excel(r'Bases de Dados\Emails.xlsx')
     lojas_df =  pd.read_csv(r'Bases de Dados\Lojas.csv', encoding='latin1', sep=';')
     vendas_df = pd.read_excel(r'Bases de Dados\Vendas.xlsx')
+
+    caminho_diretorio = Path(r"Arquivos Lojas")
 
     
 
@@ -21,6 +23,11 @@ def main():
     #Sempre pegar o maior dia 
     indicador_dia = pegar_dia(vendas)
 
+    #Se a pasta Arquivos lojas não existe, ela vai ser criada
+    if not caminho_diretorio.is_dir():
+        # Cria o diretório se não existir
+        caminho_diretorio.mkdir(parents=True, exist_ok=True)
+    
     #Criar as pastas das Lojas dentro da pasta backup arquivo Lojas
     criar_pasta_backup(dicionario_lojas, indicador_dia)
     
